fe7017293c2009d700f3831f633fd59b9e87b902
Francisco Puga <fran.puga@gmail.com>
2009-12-01 23:22:23 +0100

    Automatizando xeración de gráficos e obtención de estatísticas

--- src/charts.py
+++ src/charts.py
@@ -7,47 +7,39 @@ import math
 ROOT = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(ROOT, '..'))
 
+#http://code.google.com/intl/gl/apis/chart/
 from pygooglechart import StackedHorizontalBarChart
 
 import settings
 
-def stacked_horizontal(chapter):
+
+def changeExtension(filename, ext):
+      """ Returns a filename changing the actual extension by 'ext'. The 'filename' must have only one '.'. """
+      aux = filename.split('.')
+      return aux[0]+'.'+ext
+
+
+def main(argv):
+
+    nTraducidas = int(argv[0])
+    nFuzzy = int(argv[1])
+    nNoTraducidas = int(argv[2])
+
+    totalCadenas = nTraducidas + nFuzzy + nNoTraducidas
+    pTraducidas = (nTraducidas * 100) / totalCadenas
+    pNoTraducidas = 100 - pTraducidas
+    #print '% traducidos: ' + str(pTraducidas) + ' - % No Traducidos: ' + str(pNoTraducidas)
+
     chart = StackedHorizontalBarChart(settings.width,
                                       settings.height,
                                       x_range=(0, 100))
-#     chart.set_bar_width(100)
+    # chart.set_bar_width(100)
     chart.set_colours(['00ff00', 'ff0000'])
     chart.fill_solid('bg', 'e3d185')
     chart.set_zero_line(0, 0)
-    chart.add_data([chapter[0]])
-    chart.add_data([chapter[1]])
-    chart.download('../web/img/'+chapter[2]+'.png')
-
-def main():
-    chapters = [
-        [45, 55, 'libro'],
-        [0, 100, 'book'],
-        [100, 0, 'ch00'],
-        [100, 0, 'ch01'],
-        [89, 11, 'ch02'],
-        [71, 29, 'ch03'],
-        [0, 100, 'ch04'],
-        [100, 0, 'ch05'],
-        [0, 100, 'ch06'],
-        [0, 100, 'ch07'],
-        [0, 100, 'ch08'],
-        [100, 0, 'ch09'],
-        [0, 100, 'appa'],
-        [0, 100, 'appb'],
-        [0, 100, 'appc'],
-        [0, 100, 'appd'],
-        [0, 100, 'appe'],
-        [0, 100, 'dedi']
-        ]
-    
-    for i in range(len(chapters)):
-        stacked_horizontal(chapters[i])
+    chart.add_data([pTraducidas])
+    chart.add_data([pNoTraducidas])
+    chart.download('../web/img/' +  changeExtension(argv[3], 'png'))
 
 if __name__ == '__main__':
-    main()
-
+    main(sys.argv[1:])
--- src/get-stats-from-po.sh
+++ src/get-stats-from-po.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
 
-msgfmt --statistics -o /dev/null $1 2>/tmp/foo
+DIR='../po/gl/'
 
-echo "$1:"`cat /tmp/foo`
+SCRIPT_PYTHON='charts.py'
+DEBUG='True'
+
+totalTraducidas=0
+totalFuzzy=0
+totalNoTraducidas=0
+
+
+if [ $DEBUG == 'True' ] ; then
+    echo "fichero : traducidos : fuzzy : notraducidos"
+fi
+
+for file in `find $DIR -iname '*.po'` ; do
+
+    nameFile=`basename $file`
+    cadena=`msgfmt --statistics -o /dev/null $file 2>&1 `
+
+    traducidas=`echo $cadena | egrep '[0-9]+ translated message[s]*' -o | egrep '[0-9]+' -o`
+    fuzzy=`echo $cadena | egrep '[0-9]+ fuzzy translation[s]*' -o | egrep '[0-9]+' -o`
+    noTraducidas=`echo $cadena | egrep '[0-9]+ untranslated message[s]*' -o | egrep '[0-9]+' -o `
+
+    if [ -z $traducidas ] ; then traducidas=0 ; fi
+    if [ -z $fuzzy ] ; then fuzzy=0 ; fi
+    if [ -z $noTraducidas ] ; then noTraducidas=0 ; fi
+
+    if [ $DEBUG = 'True' ] ; then
+        echo "$nameFile : $traducidas - $fuzzy - $noTraducidas"
+    fi;
+
+    totalTraducidas=`echo $((traducidas + totalTraducidas))`
+    totalFuzzy=`echo $((fuzzy + totalFuzzy))`
+    totalNoTraducidas=`echo $((noTraducidas + totalNoTraducidas))`
+
+    python $SCRIPT_PYTHON $traducidas $fuzzy $noTraducidas $nameFile
+
+done
+
+
+
+if [ $DEBUG = 'True' ] ; then
+    echo "Total : $totalTraducidas - $totalFuzzy - $totalNoTraducidas"
+fi;
+
+python $SCRIPT_PYTHON $totalTraducidas $totalFuzzy $totalNoTraducidas 'total.po'
